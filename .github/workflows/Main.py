01607486e3369d0d0c9e7c15598a2ba0cda09498
rafa-br34 <111343658+rafa-br34@users.noreply.github.com>
2023-08-15 17:31:51 -0300

    Updated Workflow

--- .github/workflows/Main.py
+++ .github/workflows/Main.py
@@ -1,8 +1,30 @@
-import threading
+import requests
+import os
+import io
+from PIL import Image
 
+c_ImageAddress = "https://opengraph.githubassets.com/50829375aae1df9ce9fec1881829ab50997306cef04129bd38e65b2267576d63/rafa-br34/PlaceIPv6Bot"
+c_OutputName = "image.png"
+c_RunTime = "10m"
+c_RootPath = ""
 
 def main():
-	pass
+	ImageData = requests.get(c_ImageAddress)
+	if not ImageData:
+		print("Failed To Acquire Canvas Image(Exception).")
+		return exit(1)
+	elif ImageData.status_code != 200:
+		print(f"Failed To Acquire Image. Status Code {ImageData.status_code}")
+		return exit(1)
+	
+	TargetImage = Image.open(io.BytesIO(ImageData.content))
+	NewSize = max(*TargetImage.size)
+	NewImage = Image.new("RGB", [NewSize, NewSize], color=0xFFFFFF)
+	NewImage.paste(TargetImage, [0, int((NewSize / 2) - (TargetImage.size[1] / 2))])
+	NewImage.save(f"{c_RootPath}/{c_OutputName}")
+	os.system(f"timeout -k {c_RunTime} {c_RunTime} python3 {c_RootPath}/Main.py")
+
+
 
 if __name__ == '__main__':
 	main()
